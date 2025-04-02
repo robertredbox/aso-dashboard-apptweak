f1f013d46a09b71c1f7783971e94f6d9273d16c0
robertredbox <robert@redboxmobile.com>
2025-04-02 11:06:15 +0100

    Add AuthLayout component

--- client/src/layouts/AuthLayout.js
+++ client/src/layouts/AuthLayout.js
@@ -5,11 +5,9 @@ const AuthLayout = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Link to="/">
-          <h2 className="mt-6 text-center text-3xl font-slab font-medium text-primary-600">
-            ASO Dashboard
-          </h2>
-        </Link>
+        <h2 className="mt-6 text-center text-3xl font-slab font-medium text-primary-600">
+          ASO Dashboard
+        </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
           App Store Optimization with AppTweak
         </p>
